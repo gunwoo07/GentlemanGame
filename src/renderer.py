0ff949e03e538842be40b687f4dd0f1f0bdac051
rainmaker <paul37george@gmail.com>
2026-05-21 19:44:31 +0900

    rendere2

--- src/renderer.py
+++ src/renderer.py
@@ -1,5 +1,6 @@
 import pygame
 import map
+from config import *
 
 class Renderer:
     def __init__(self, screen):
@@ -7,16 +8,47 @@ class Renderer:
     
     def render(self, game_state):
         self.screen.fill((75, 0, 130)) # indigo blue
-        self.draw_map(game_state.map)
-    
+        self.draw_map(game_state['map'])
+        self.draw_towers(game_state['towers'])
+        self.draw_enemies(game_state['enemies'])
+        self.draw_stat(game_state['stat'])
+        
     def draw_map(self, game_map):
         for i in range(map.ROWS):
             for j in range(map.COLS):
                 if game_map[i][j] == 0:
-                    pygame.draw.rect(self.screen, 'lightgreen', (*map.get_pos(i, j), map.TILE_SIZE, map.TILE_SIZE))
+                    pygame.draw.rect(self.screen, 'lightgreen', (*map.get_pos(i, j), TILE_SIZE, TILE_SIZE))
                 if game_map[i][j] == 1:
-                    pygame.draw.rect(self.screen, 'brown', (*map.get_pos(i, j), map.TILE_SIZE, map.TILE_SIZE))
+                    pygame.draw.rect(self.screen, 'brown', (*map.get_pos(i, j), TILE_SIZE, TILE_SIZE))
                 elif game_map[i][j] == 2:
-                    pygame.draw.rect(self.screen, 'red', (*map.get_pos(i, j), map.TILE_SIZE, map.TILE_SIZE))
+                    pygame.draw.rect(self.screen, 'red', (*map.get_pos(i, j), TILE_SIZE, TILE_SIZE))
                 elif game_map[i][j] == 3:
-                    pygame.draw.rect(self.screen, 'green', (*map.get_pos(i, j), map.TILE_SIZE, map.TILE_SIZE))
+                    pygame.draw.rect(self.screen, 'green', (*map.get_pos(i, j), TILE_SIZE, TILE_SIZE))
+    
+    def draw_towers(self, towers):
+        for tower in towers:
+            tower.draw(self.screen)
+
+    def draw_enemies(self, enemies):
+        for enemy in enemies:
+            enemy.draw(self.screen)
+
+    def draw_stat(self, stat):
+        pygame.draw.rect(self.screen, 'black', (MARGIN, MARGIN*2+MAP_HEIGHT, STAT_WIDTH, STAT_HEIGHT))
+        font = pygame.font.SysFont("malgungothic", 24)
+        text = font.render(f"골드: {stat['gold']}  HP: {stat['hp']}  웨이브: {stat['wave']}/{stat['max_wave']}", True, 'white')
+        self.screen.blit(text, (MARGIN*2, MARGIN*2+MAP_HEIGHT+STAT_HEIGHT/4))
+
+"""
+{
+    "map":
+    "towers":
+    "enemies":
+    "stat": {
+        "gold":
+        "hp":
+        "wave":
+        "max_wave":
+    }
+}
+"""
